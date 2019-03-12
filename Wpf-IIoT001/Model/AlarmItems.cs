e0a0dbaae8e81ec034abc1e4ada75ccedfcc11fb
Administrator <Administrator@DESKTOP-0JALOGJ>
2019-03-12 16:59:23 +0800

    修改了机器状态Item的添加方式

--- Wpf-IIoT001/Model/AlarmItems.cs
+++ Wpf-IIoT001/Model/AlarmItems.cs
@@ -14,83 +14,96 @@ namespace Wpf_IIoT001
         {
             //第一排
             //DF07 Index:1
-            AddDFAlarmItems("制造车间", "大机#07", "#01", 1);
+            AddAlarmItems("制造车间","DF", "大机#07", "#01", (int)MachineIndex.DF07);
             //DF06 Index:2
-            AddDFAlarmItems("制造车间", "大机#06", "#01", 2);
+            AddAlarmItems("制造车间", "DF", "大机#06", "#01", (int)MachineIndex.DF06);
             //SF08 Index:3
-            AddSFAlarmItems("制造车间", "小机#08", "#01", 3);
+            AddAlarmItems("制造车间", "SF", "小机#08", "#01", (int)MachineIndex.SF08);
             //SF07 Index:4
             //_machineFlagDict.Add("制造车间小机#07.#01.状态.机器运行标志", 301);
-            AddSFAlarmItems("制造车间", "小机#07", "#01", 4);
+            AddAlarmItems("制造车间", "SF", "小机#07", "#01", (int)MachineIndex.SF07);
             //SF06 Index:5
-            AddSFAlarmItems("制造车间", "小机#06", "#01", 5);
+            AddAlarmItems("制造车间", "SF", "小机#06", "#01", (int)MachineIndex.SF06);
             //SF05 Index:6
-            AddSFAlarmItems("制造车间", "小机#05", "#01", 6);
+            AddAlarmItems("制造车间", "SF", "小机#05", "#01", (int)MachineIndex.SF05);
             //SF04 Index:7
-            AddSFAlarmItems("制造车间", "小机#04", "#01", 7);
+            AddAlarmItems("制造车间", "SF", "小机#04", "#01", (int)MachineIndex.SF04);
             //SF03 Index:8
-            AddSFAlarmItems("制造车间", "小机#03", "#01", 8);
+            AddAlarmItems("制造车间", "SF", "小机#03", "#01", (int)MachineIndex.SF03);
             //SF02 Index:9
-            AddSFAlarmItems("制造车间", "小机#02", "#01", 9);
+            AddAlarmItems("制造车间", "SF", "小机#02", "#01", (int)MachineIndex.SF02);
             //SF01 Index:10
-            AddSFAlarmItems("制造车间", "小机#01", "#01", 10);
+            AddAlarmItems("制造车间", "SF", "小机#01", "#01", (int)MachineIndex.SF01);
             //DF05 Index:11
-            AddDFAlarmItems("制造车间", "大机#05", "#01", 11);
+            AddAlarmItems("制造车间", "DF", "大机#05", "#01", (int)MachineIndex.DF05);
             //DF04 Index:12
-            AddDFAlarmItems("制造车间", "大机#04", "#01", 12);
+            AddAlarmItems("制造车间", "DF", "大机#04", "#01", (int)MachineIndex.DF04);
             //DF03 Index:13
-            AddDFAlarmItems("制造车间", "大机#03", "#01", 13);
+            AddAlarmItems("制造车间", "DF", "大机#03", "#01", (int)MachineIndex.DF03);
             //DF02 Index:14
-            AddDFAlarmItems("制造车间", "大机#02", "#01", 14);
+            AddAlarmItems("制造车间", "DF", "大机#02", "#01", (int)MachineIndex.DF02);
             //DF01 Index:15
-            AddDFAlarmItems("制造车间", "大机#01", "#01", 15);
+            AddAlarmItems("制造车间", "DF", "大机#01", "#01", (int)MachineIndex.DF01);
 
             //第二排
             //DF17 Index:16
-            AddDFAlarmItems("制造车间", "大机#17", "#01", 16);
+            AddAlarmItems("制造车间", "DF", "大机#17", "#01", (int)MachineIndex.DF17);
             //DF16 Index:17
-            AddDFAlarmItems("制造车间", "大机#16", "#01", 17);
+            AddAlarmItems("制造车间", "DF", "大机#16", "#01", (int)MachineIndex.DF16);
             //DF15 Index:18
-            AddDFAlarmItems("制造车间", "大机#15", "#01", 18);
+            AddAlarmItems("制造车间", "DF", "大机#15", "#01", (int)MachineIndex.DF15);
             //SF12 Index:19
-            AddSFAlarmItems("制造车间", "小机#12", "#01", 19);
+            AddAlarmItems("制造车间", "SF", "小机#12", "#01", (int)MachineIndex.SF12);
             //SF11 Index:20
-            AddSFAlarmItems("制造车间", "小机#11", "#01", 20);
+            AddAlarmItems("制造车间", "SF", "小机#11", "#01", (int)MachineIndex.SF11);
             //SF10 Index:21
-            AddSFAlarmItems("制造车间", "小机#10", "#01", 21);
+            AddAlarmItems("制造车间", "SF", "小机#10", "#01", (int)MachineIndex.SF10);
             //SF09 Index:22
-            AddSFAlarmItems("制造车间", "小机#09", "#01", 22);
+            AddAlarmItems("制造车间", "SF", "小机#09", "#01", (int)MachineIndex.SF09);
             //DF14 Index:23
-            AddDFAlarmItems("制造车间", "大机#14", "#01", 23);
+            AddAlarmItems("制造车间", "DF", "大机#14", "#01", (int)MachineIndex.SF08);
             //DF13 Index:24
-            AddDFAlarmItems("制造车间", "大机#13", "#01", 24);
+            AddAlarmItems("制造车间", "DF", "大机#13", "#01", (int)MachineIndex.DF13);
             //DF12 Index:25
-            AddDFAlarmItems("制造车间", "大机#12", "#01", 25);
+            AddAlarmItems("制造车间", "DF", "大机#12", "#01", (int)MachineIndex.DF12);
             //DF11 Index:26
-            AddDFAlarmItems("制造车间", "大机#11", "#01", 26);
+            AddAlarmItems("制造车间", "DF", "大机#11", "#01", (int)MachineIndex.DF11);
             //DF10 Index:27
-            AddDFAlarmItems("制造车间", "大机#10", "#01", 27);
+            AddAlarmItems("制造车间", "DF", "大机#10", "#01", (int)MachineIndex.DF10);
             //DF09 Index:28
-            AddDFAlarmItems("制造车间", "大机#09", "#01", 28);
+            AddAlarmItems("制造车间", "DF", "大机#09", "#01", (int)MachineIndex.DF09);
             //DF08 Index:29
-            AddDFAlarmItems("制造车间", "大机#08", "#01", 29);
+            AddAlarmItems("制造车间", "DF", "大机#08", "#01", (int)MachineIndex.DF08);
 
             //第三排
             //SF13 Index:30
-            AddSFAlarmItems("制造车间", "小机#13", "#01", 30);
+            AddAlarmItems("制造车间", "SF", "小机#13", "#01", (int)MachineIndex.SF13);
             //SF14 Index:31
-            AddSFAlarmItems("制造车间", "小机#14", "#01", 31);
+            AddAlarmItems("制造车间", "SF", "小机#14", "#01", (int)MachineIndex.SF14);
             //DF19 Index:32
-            //AddDFAlarmItems("制造车间", "大机#19", "#01", 31);
+            //AddAlarmItems("制造车间","DF", "大机#19", "#01", 31);
             //SE13 Index:33
-            AddSFAlarmItems("制造车间", "手吹小机#13", "#01", 33);
+            AddAlarmItems("制造车间", "SE", "手吹小机#13", "#01", (int)MachineIndex.SE13);
         }
 
 
-        private void AddDFAlarmItems(string workshop, string machineNo, string plcNo, int index)
+        private void AddAlarmItems(string workshop,string machineType, string machineNo, string plcNo, int index)
         {
-            //遍历Enum获取Descriprion
-            Type type = typeof(AlarmInfoOfDF);
+            Type type=null;
+            //遍历Enum获取并添加所有报警信息
+            switch(machineType)
+            {
+                case "DF":
+                    type = typeof(AlarmInfoOfDF);
+                    break;
+                case "SF":
+                    type = typeof(AlarmInfoOfSF);
+                    break;
+                case "SE":
+                    type = typeof(AlarmInfoOfSE);
+                    break;
+            }
+            
 
             foreach (FieldInfo x in type.GetFields(BindingFlags.Public | BindingFlags.Static))
             {
@@ -116,34 +129,6 @@ namespace Wpf_IIoT001
             }
         }
 
-        private void AddSFAlarmItems(string workshop, string machineNo, string plcNo, int index)
-        {
-            Type type = typeof(AlarmInfoOfSF);
-
-            foreach (FieldInfo x in type.GetFields(BindingFlags.Public | BindingFlags.Static))
-            {
-                string _handleName;
-                string _description = string.Empty;
-                object[] array = x.GetCustomAttributes(typeof(DescriptionAttribute), false);
-                if (array.Length > 0)
-                {
-                    _description = ((DescriptionAttribute)array[0]).Description;
-                    //Console.WriteLine(description);
-                }
-                else
-                {
-                    _description = ""; //none description,set empty
-                }
-                _handleName = workshop + machineNo + "." + plcNo + ".报警信息." + _description;
-                _alarmFlagDict.Add(_handleName, index * 10000 + (int)x.GetValue(null));
-                //初始化报警信息List
-                AlarmMessage alarmMessage = new AlarmMessage();
-                alarmMessage.MachineNo = machineNo;
-                alarmMessage.Index = index * 10000 + (int)x.GetValue(null);
-                alarmMessage.AlarmMessages = _description;
-                GlobalVars.alarmMessages.Add(alarmMessage);
-            }
-        }
 
         public Dictionary<string, int> getAlarmFlagDict()
         {
--- Wpf-IIoT001/Model/machineItems.cs
+++ Wpf-IIoT001/Model/machineItems.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,136 +18,111 @@ namespace Wpf_IIoT001
         {
             //第一排
             //DF07 Index:0
-            AddS7_200Items("制造车间", "大机#07", "#01", (int)MachineIndex.DF07);
+            AddStatusItems("制造车间","S7-200", "大机#07", (int)MachineIndex.DF07);
             //DF06 Index:1
-            AddS7_200Items("制造车间", "大机#06", "#01", (int)MachineIndex.DF06);
+            AddStatusItems("制造车间", "S7-200", "大机#06", (int)MachineIndex.DF06);
             //SF08 Index:2
-            AddS7_200Items("制造车间", "小机#08", "#01", (int)MachineIndex.SF08);
+            AddStatusItems("制造车间", "S7-200", "小机#08",(int)MachineIndex.SF08);
             //SF07 Index:3
             //_machineFlagDict.Add("制造车间小机#07.#01.状态.机器运行标志", 301);
-            AddS7_200Items("制造车间", "小机#07", "#01", (int)MachineIndex.SF07);
+            AddStatusItems("制造车间","S7-200", "小机#07", (int)MachineIndex.SF07);
             //SF06 Index:4
-            AddS7_200Items("制造车间", "小机#06", "#01", (int)MachineIndex.SF06);
+            AddStatusItems("制造车间", "S7-200", "小机#06",  (int)MachineIndex.SF06);
             //SF05 Index:5
-            AddS7_200Items("制造车间", "小机#05", "#01", (int)MachineIndex.SF05);
+            AddStatusItems("制造车间", "S7-200", "小机#05",  (int)MachineIndex.SF05);
             //SF04 Index:6
-            AddS7_200Items("制造车间", "小机#04", "#01", (int)MachineIndex.SF04);
+            AddStatusItems("制造车间", "S7-200", "小机#04",  (int)MachineIndex.SF04);
             //SF03 Index:7
-            AddS7_200Items("制造车间", "小机#03", "#01", (int)MachineIndex.SF03);
+            AddStatusItems("制造车间", "S7-200", "小机#03",  (int)MachineIndex.SF03);
             //SF02 Index:8
-            AddS7_200Items("制造车间", "小机#02", "#01", (int)MachineIndex.SF02);
+            AddStatusItems("制造车间", "S7-200", "小机#02",  (int)MachineIndex.SF02);
             //SF01 Index:9
-            AddS7_200Items("制造车间", "小机#01", "#01", (int)MachineIndex.SF01);
+            AddStatusItems("制造车间", "S7-200", "小机#01",  (int)MachineIndex.SF01);
             //DF05 Index:10
-            AddS7_200Items("制造车间", "大机#05", "#01", (int)MachineIndex.DF05);
+            AddStatusItems("制造车间", "S7-200", "大机#05",  (int)MachineIndex.DF05);
             //DF04 Index:11
-            AddS7_200Items("制造车间", "大机#04", "#01", (int)MachineIndex.DF04);
+            AddStatusItems("制造车间", "S7-200", "大机#04",  (int)MachineIndex.DF04);
             //DF03 Index:12
-            AddS7_200Items("制造车间", "大机#03", "#01", (int)MachineIndex.DF03);
+            AddStatusItems("制造车间", "S7-200", "大机#03",  (int)MachineIndex.DF03);
             //DF02 Index:13
-            AddS7_200Items("制造车间", "大机#02", "#01", (int)MachineIndex.DF02);
+            AddStatusItems("制造车间", "S7-200","大机#02",  (int)MachineIndex.DF02);
             //DF01 Index:14
-            AddS7_200Items("制造车间", "大机#01", "#01", (int)MachineIndex.DF01);
+            AddStatusItems("制造车间", "S7-200","大机#01",  (int)MachineIndex.DF01);
 
             //第二排
             //DF17 Index:15
-            AddS7_200Items("制造车间", "大机#17", "#01", (int)MachineIndex.DF17);
+            AddStatusItems("制造车间", "S7-200", "大机#17",  (int)MachineIndex.DF17);
             //DF16 Index:16
-            AddS7_200Items("制造车间", "大机#16", "#01", (int)MachineIndex.DF16);
+            AddStatusItems("制造车间", "S7-200","大机#16",  (int)MachineIndex.DF16);
             //DF15 Index:17
-            AddS7_200Items("制造车间", "大机#15", "#01", (int)MachineIndex.DF15);
+            AddStatusItems("制造车间", "S7-200", "大机#15",  (int)MachineIndex.DF15);
             //SF12 Index:18
-            AddS7_200Items("制造车间", "小机#12", "#01", (int)MachineIndex.SF12);
+            AddStatusItems("制造车间", "S7-200", "小机#12",  (int)MachineIndex.SF12);
             //SF11 Index:19
-            AddS7_200Items("制造车间", "小机#11", "#01", (int)MachineIndex.SF11);
+            AddStatusItems("制造车间", "S7-200", "小机#11",  (int)MachineIndex.SF11);
             //SF10 Index:20
-            AddS7_200Items("制造车间", "小机#10", "#01", (int)MachineIndex.SF10);
+            AddStatusItems("制造车间", "S7-200","小机#10",  (int)MachineIndex.SF10);
             //SF09 Index:21
-            AddS7_200Items("制造车间", "小机#09", "#01", (int)MachineIndex.SF09);
+            AddStatusItems("制造车间", "S7-200","小机#09",  (int)MachineIndex.SF09);
             //DF14 Index:22
-            AddS7_200Items("制造车间", "大机#14", "#01", (int)MachineIndex.DF14);
+            AddStatusItems("制造车间", "S7-200","大机#14",  (int)MachineIndex.DF14);
             //DF13 Index:23
-            AddS7_200Items("制造车间", "大机#13", "#01", (int)MachineIndex.DF13);
+            AddStatusItems("制造车间", "S7-200", "大机#13",  (int)MachineIndex.DF13);
             //DF12 Index:24
-            AddS7_200Items("制造车间", "大机#12", "#01", (int)MachineIndex.DF12);
+            AddStatusItems("制造车间", "S7-200", "大机#12",  (int)MachineIndex.DF12);
             //DF11 Index:25
-            AddS7_200Items("制造车间", "大机#11", "#01", (int)MachineIndex.DF11);
+            AddStatusItems("制造车间", "S7-200","大机#11",  (int)MachineIndex.DF11);
             //DF10 Index:26
-            AddS7_200Items("制造车间", "大机#10", "#01", (int)MachineIndex.DF10);
+            AddStatusItems("制造车间", "S7-200","大机#10",  (int)MachineIndex.DF10);
             //DF09 Index:27
-            AddS7_200Items("制造车间", "大机#09", "#01", (int)MachineIndex.DF09);
+            AddStatusItems("制造车间", "S7-200","大机#09",  (int)MachineIndex.DF09);
             //DF08 Index:28
-            AddS7_200Items("制造车间", "大机#08", "#01", (int)MachineIndex.DF08);
+            AddStatusItems("制造车间", "S7-200","大机#08",  (int)MachineIndex.DF08);
 
             //第三排
             //SF13 Index:29
-            AddS7_200Items("制造车间", "小机#13", "#01", (int)MachineIndex.SF13);
+            AddStatusItems("制造车间", "S7-200","小机#13",  (int)MachineIndex.SF13);
             //SF14 Index:30
-            AddS7_200Items("制造车间", "小机#14", "#01", (int)MachineIndex.SF14);
+            AddStatusItems("制造车间", "S7-200","小机#14",  (int)MachineIndex.SF14);
             //DF19 Index:31
-            AddS7_1200Items("制造车间", "大机#19", "#01", (int)MachineIndex.DF19);
+            AddStatusItems("制造车间", "S7-1200", "大机#19",  (int)MachineIndex.DF19);
             //SE13 Index:32
-            AddS7_200Items("制造车间", "手吹小机#13", "#01", (int)MachineIndex.SE13);
+            AddStatusItems("制造车间", "S7-200","手吹小机#13",  (int)MachineIndex.SE13);
         }
 
-        private void AddS7_200Items(string workshop,string machineNo,string plcNo,int index)
+        private void AddStatusItems(string workshop, string plcType, string machineNo, int index)
         {
-            string _handleName;
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.机器运行标志";
-            _machineFlagDict.Add(_handleName, index * 100 + 1);
+            Type type = null;
+            //遍历Enum获取并添加所有报警信息
+            switch (plcType)
+            {
+                case "S7-200":
+                    type = typeof(S7200StatusInfo);
+                    break;
+                case "S7-1200":
+                    type = typeof(S71200StatusInfo);
+                    break;
+            }
 
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.炉子电源开关";
-            _machineFlagDict.Add(_handleName, index * 100 + 2);
 
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.升料机开关";
-            _machineFlagDict.Add(_handleName, index * 100 + 3);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".报警信息.报警提示";
-            _machineFlagDict.Add(_handleName, index * 100 + 4);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.烤模时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 5);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.浸料时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 6);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.烤料时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 7);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.冷却时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 8);
+            foreach (FieldInfo x in type.GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                string _handleName;
+                string _description = string.Empty;
+                object[] array = x.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (array.Length > 0)
+                {
+                    _description = ((DescriptionAttribute)array[0]).Description;
+                }
+                else
+                {
+                    _description = ""; //none description,set empty
+                }
+                _handleName = workshop + machineNo + "." + _description;
+                _machineFlagDict.Add(_handleName, index * 100 + (int)x.GetValue(null));
+            }
         }
 
-        private void AddS7_1200Items(string workshop, string machineNo, string plcNo, int index)
-        {
-            string _handleName;
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.机器运行标志";
-            _machineFlagDict.Add(_handleName, index * 100 + 1);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.烤模炉子电源开关";
-            _machineFlagDict.Add(_handleName, index * 100 + 2);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.升料机开关";
-            _machineFlagDict.Add(_handleName, index * 100 + 3);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".报警信息.报警提示";
-            _machineFlagDict.Add(_handleName, index * 100 + 4);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.烤模时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 5);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.浸料时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 6);
-
-            _handleName = workshop + machineNo + ".#02" + ".状态.烤料时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 7);
-
-            _handleName = workshop + machineNo + ".#02" + ".状态.冷却时间设定";
-            _machineFlagDict.Add(_handleName, index * 100 + 8);
-
-            _handleName = workshop + machineNo + "." + plcNo + ".状态.烤料炉子电源开关";
-            _machineFlagDict.Add(_handleName, index * 100 + 9);
-        }
         public Dictionary<string, int> getMachineFlagDict()
         {
             return _machineFlagDict;
